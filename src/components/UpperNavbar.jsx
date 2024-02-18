114eb012b9db329258286515f7eb114e6c9ac04a
Param Shelar <shelarparam@gmail.com>
2024-02-18 18:30:25 +0530

    login modal image

--- src/components/UpperNavbar.jsx
+++ src/components/UpperNavbar.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { Box, Button } from "@mui/material";
+import twitterImg from "./assets/twitter.png";
+import discordImg from "./assets/discord.png";
 
 export default function UpperNavbar() {
   return (
     <Box
       style={{
         backgroundColor: "#0D0B1B",
-        borderRadius: 20,
+        borderBottomLeftRadius: "20px",
         paddingLeft: "0",
       }}
       sx={{ width: "100%", height: "25%", alignSelf: "baseline" }}
     >
       <Button sx={{ p: 1 }}> 1K Raffel</Button>
+      <Button sx={{ p: 1 }}> EVENT</Button>
+      <Button sx={{ p: 1 }}> Affiliates</Button>
+      <Button sx={{ p: 1 }}> Game Responsibly</Button>
+      <Button sx={{ p: 1 }}>Fairness</Button>
+      <Button sx={{ p: 1 }}>FAQ</Button>
+      <Button sx={{ p: 1 }}>Terms of Service</Button>
+      <Button sx={{ p: 1 }}>Claims</Button>
+      <Button sx={{ p: 1 }}>
+        <img src={twitterImg}></img>
+      </Button>
+      <Button sx={{ p: 1 }}>
+        <img src={discordImg}></img>
+      </Button>
     </Box>
   );
 }
