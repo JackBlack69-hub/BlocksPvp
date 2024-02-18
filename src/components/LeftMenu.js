06808c8abd64773707098ecf0c446551c10d04a4
Param Shelar <shelarparam@gmail.com>
2024-02-18 10:16:18 +0530

    icons left

--- src/components/LeftMenu.js
+++ src/components/LeftMenu.js
@@ -1,33 +1,36 @@
 // LeftMenu.js
-import React from 'react';
-import { Drawer, List, ListItem, ListItemText } from '@mui/material';
+import React from "react";
+import { Drawer, List, ListItem, ListItemText } from "@mui/material";
+import coinflipImg from "../components/coinflip.png";
+import jackpotImg from "../components/jackpot.png";
 
 function LeftMenu({ open, onClose }) {
   return (
     <Drawer
       variant="permanent"
       anchor="left"
-      style={{ zIndex: '-1', position: 'relative', backgroundColor: '#19172C' }}
+      style={{ zIndex: "-1", position: "relative", backgroundColor: "#19172C" }}
       PaperProps={{
         sx: {
-          backgroundColor: "#19172C", // Your desired color   
+          backgroundColor: "#19172C", // Your desired color
         },
       }}
     >
-      <List style={{ marginTop: '130%' }}>
-        <ListItem style={{ color: '#9F9DA7' }} button>
-          <ListItemText primary="Coinflip" />
+      <List style={{ marginTop: "30%" }}>
+        <ListItem style={{ color: "#9F9DA7", paddingLeft: "0" }} button>
+          {/* <ListItemText primary="Coinflip" /> */}
+          <img src={coinflipImg}></img>
         </ListItem>
-        <ListItem style={{ color: '#9F9DA7' }} button>
-          <ListItemText primary="Jackpot" />
+        <ListItem style={{ color: "#9F9DA7", paddingLeft: "20%" }} button>
+          <img src={jackpotImg}></img>
         </ListItem>
       </List>
 
-      <List style={{ marginTop: 'auto' }}>
-        <ListItem style={{ color: '#9F9DA7' }} button>
+      <List style={{ marginTop: "auto" }}>
+        <ListItem style={{ color: "#9F9DA7" }} button>
           <ListItemText primary="Discord" />
         </ListItem>
-        <ListItem style={{ color: '#9F9DA7' }} button>
+        <ListItem style={{ color: "#9F9DA7" }} button>
           <ListItemText primary="Support" />
         </ListItem>
       </List>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,31 @@
 // Navbar.js
-import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
-import logo from "./Logopvp.png"
+import React from "react";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import logo from "./Logopvp.png";
+import { sizing } from "@mui/system";
 
 function Navbar() {
   return (
-    <AppBar position="static" style={{ backgroundColor: '#19172C', paddingRight: '0', paddingLeft: '0', display: 'block' }}>
-      <Toolbar style={{paddingRight: '0', paddingLeft: '0' }}>
+    <AppBar
+      position="static"
+      style={{
+        backgroundColor: "#19172C",
+        paddingRight: "0",
+        paddingLeft: "0",
+        display: "block",
+      }}
+    >
+      <Toolbar style={{ paddingRight: "0", paddingLeft: "0" }}>
         <img src={logo} />
-        <div>
-          CoinFlip
-        </div>
-        <div style={{backgroundColor: '#0D0B1B', width: '100%',alignSelf: 'baseline'}}> 
-          1K Raffel
-        </div>
+
+        <Box
+          style={{
+            backgroundColor: "#0D0B1B",
+          }}
+          sx={{ width: "100%", height: "25%", alignSelf: "baseline" }}
+        >
+          <Box sx={{ p: 1 }}> 1K Raffel</Box>
+        </Box>
       </Toolbar>
     </AppBar>
   );
