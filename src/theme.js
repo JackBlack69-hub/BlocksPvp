db5a2748690698696edf77d8dcdb036e30bad74b
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-02-24 22:13:12 +0530

    feat: added poppins font to mui

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,26 @@
 // theme.js
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
+  typography: {
+    fontFamily: `
+        "Poppins",
+        -apple-system,
+        BlinkMacSystemFont,
+        "Segoe UI",
+        "Roboto",
+        "Oxygen",
+        "Ubuntu",
+        "Cantarell",
+        "Fira Sans",
+        "Droid Sans",
+        "Helvetica Neue",
+        sans-serif
+    `,
+  },
   palette: {
     primary: {
-      main: '#2196f3',
+      main: "#2196f3",
     },
   },
 });
