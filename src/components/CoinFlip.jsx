3e8db9d5fea6201be139c284727c1f45854d5e7e
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-03-02 21:23:07 +0530

    feat: created coinflilp menu items

--- src/components/CoinFlip.jsx
+++ src/components/CoinFlip.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import HTCombineImg from "./assets/Combine.png";
 import CreateGameModal from "./CreateGameModal";
+import playerImage1 from "./assets/p1.png";
+import playerImage2 from "./assets/player.png";
+import pumpkinImage from "./assets/pumpkin.png";
 import styles from "./CoinFlip.module.css";
 
 function CoinFlip() {
+  const [gameList, setGameList] = useState([
+    {
+      isGameStarted: true,
+    },
+    {
+      isGameStarted: false,
+    },
+  ]);
+
   return (
     <div style={{ margin: "2rem 3rem" }}>
       <div
@@ -104,40 +116,117 @@ function CoinFlip() {
           Test
         </div>
       </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "100%",
-          background: "linear-gradient(180deg, #19182C 0%, #2F214F 100%)",
-          color: "white",
-          padding: "2%",
-          marginTop: "1%",
-        }}
-      >
-        <div>Text</div>
-        <div>Text</div>
-        <div>Text</div>
-        <div>Text</div>
-        <div>Text</div>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "100%",
-          background: "linear-gradient(180deg, #19182C 0%, #2F214F 100%)",
-          color: "white",
-          padding: "2%",
-          marginTop: "1%",
-        }}
-      >
-        <div>Text</div>
-        <div>Text</div>
-        <div>Text</div>
-        <div>Text</div>
-        <div>Text</div>
-      </div>
+
+      {gameList.map((game) => (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+            background: "linear-gradient(180deg, #19182C 0%, #2F214F 100%)",
+            color: "white",
+            padding: "2%",
+            marginTop: "1%",
+            borderRadius: "15px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "1rem",
+            }}
+          >
+            <div className="player1">
+              <img src={playerImage1} alt="player 1"></img>
+            </div>
+            <span>vs</span>
+            <div className="player2">
+              <img src={playerImage2} alt="player 2"></img>
+            </div>
+          </div>
+          <div>
+            <img src={playerImage1} alt="coin"></img>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "1rem",
+              borderRadius: "128px",
+              background: "#151223",
+              boxShadow: "0px 9px 11.3px 0px rgba(0, 0, 0, 0.20) inset",
+              padding: "0.5rem 2rem",
+            }}
+          >
+            {[1, 2, 3, 4, 5, 6].map((key) => (
+              <div key={key} style={{ display: "flex" }}>
+                <img src={pumpkinImage} alt="pumpkin"></img>
+              </div>
+            ))}
+            <div style={{ display: "flex" }}>
+              <p>+4</p>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <p
+              style={{
+                margin: 0,
+                fontSize: "1.2rem",
+                fontWeight: 500,
+                color: "#863AFF",
+              }}
+            >
+              5,000 R$
+            </p>
+
+            <p
+              style={{
+                margin: 0,
+                fontSize: "0.9rem",
+              }}
+            >
+              (4630-5720)
+            </p>
+          </div>
+          <div
+            style={{
+              width: "160px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {game.isGameStarted ? (
+              <div>View</div>
+            ) : (
+              <button
+                style={{
+                  background: `
+                        linear-gradient(to bottom, #873AFF, #873AFF ) padding-box, 
+                        linear-gradient(#fd8eff 0%, #8c3cff 100%) border-box
+                    `,
+                  borderWidth: "2px",
+                  borderStyle: "solid",
+                  borderImageSlice: 1,
+                  borderRadius: "8px",
+                  fontWeight: 500,
+                }}
+                className={styles.joinBtn}
+              >
+                Join
+              </button>
+            )}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
